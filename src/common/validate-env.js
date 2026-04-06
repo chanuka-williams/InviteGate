5b6763bec8e22c4012d25a5c5bd7b5aa9fe9e55e
chanu <chanuka.williams@outlook.com>
2026-04-07 00:00:23 +0100

    discord invite expiry time now no longer part of env file.

--- src/common/validate-env.js
+++ src/common/validate-env.js
@@ -1,10 +1,5 @@
 export function validateEnv() {
-  const requiredEnvVars = [
-    "BOT_TOKEN",
-    "APPLICATION_ID",
-    "DEV_GUILD_ID",
-    "DISCORD_INVITE_EXPIRE_TIME",
-  ];
+  const requiredEnvVars = ["BOT_TOKEN", "APPLICATION_ID", "DEV_GUILD_ID"];
   for (const key of requiredEnvVars) {
     if (!process.env[key]) {
       throw new Error(`Missing ${key} in .env`);
