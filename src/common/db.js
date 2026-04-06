1a8efb204f891711de8bf336c98746abc15c8c60
chanu <chanuka.williams@outlook.com>
2026-04-07 00:00:53 +0100

    created functions to interacte with guild table.

--- src/common/db.js
+++ src/common/db.js
@@ -2,16 +2,22 @@ const fs = require("fs");
 const Database = require("better-sqlite3");
 
 if (!fs.existsSync("./data")) fs.mkdirSync("./data");
-const db = new Database("./data/invites.db");
+const db = new Database("./data/bot.db");
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS invites (
-    user_id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    guild_id TEXT NOT NULL,
     invite_code TEXT NOT NULL,
-    guild_id TEXT NOT NULL
-  )
+    PRIMARY KEY (user_id, guild_id)
+  );
+  CREATE TABLE IF NOT EXISTS guild_settings (
+    guild_id TEXT PRIMARY KEY,
+    invite_max_age INTEGER NOT NULL DEFAULT 86400
+  );
 `);
 
+// -- Invites --
 function getInvite(userId, guildId) {
   return (
     db
@@ -21,13 +27,11 @@ function getInvite(userId, guildId) {
       .get(userId, guildId) ?? null
   );
 }
-
 function setInvite(userId, guildId, inviteCode) {
   db.prepare(
     "INSERT OR REPLACE INTO invites (user_id, guild_id, invite_code) VALUES (?, ?, ?)",
   ).run(userId, guildId, inviteCode);
 }
-
 function deleteInvite(userId, guildId) {
   db.prepare("DELETE FROM invites WHERE user_id = ? AND guild_id = ?").run(
     userId,
@@ -35,4 +39,30 @@ function deleteInvite(userId, guildId) {
   );
 }
 
-module.exports = { getInvite, setInvite, deleteInvite };
+// -- Guild Settings --
+function initGuildSettings(guildId) {
+  db.prepare("INSERT OR IGNORE INTO guild_settings (guild_id) VALUES (?)").run(
+    guildId,
+  );
+}
+function getGuildSettings(guildId) {
+  return (
+    db
+      .prepare("SELECT * FROM guild_settings WHERE guild_id = ?")
+      .get(guildId) ?? null
+  );
+}
+function setInviteMaxAge(guildId, maxAge) {
+  db.prepare(
+    "UPDATE guild_settings SET invite_max_age = ? WHERE guild_id = ?",
+  ).run(maxAge, guildId);
+}
+
+module.exports = {
+  getInvite,
+  setInvite,
+  deleteInvite,
+  initGuildSettings,
+  getGuildSettings,
+  setInviteMaxAge,
+};
